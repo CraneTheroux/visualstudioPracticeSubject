3b6e91aa49361a7f57354ce654b1cb2cc4d15e17
Austin Crane <2640082560@qq.com>
2026-04-18 17:50:54 +0800

    stack，queue，list learning

---
+++ Project1/day6.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<stack>
+#include<queue>
+#include<list>
+#define MAX 100
+int main()
+{
+	
+	
+	std::list<int>l1;
+	for (int i = 0; i < MAX; ++i)
+	{
+		l1.push_back(i);
+
+	}
+	for (std::list<int>::iterator it = l1.begin(); it!=l1.end(); it++)
+	{
+		std::cout << *it<< " ";
+	}
+
+	std::list<int>l2;
+	for (int i = 0; i < MAX; ++i)
+	{
+		l2.push_front(i);
+	}
+	std::cout << "-----------------------------------------" << std::endl;
+	for (std::list<int>::iterator it = l2.begin(); it != l2.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	
+	
+
+}
--- Project1/test1.cpp
+++ Project1/test1.cpp
@@ -1,5 +1,21 @@
 ﻿#include<iostream>
+#include<deque>
+#define MAX 100
 using namespace std;
-int main() {
-	cout << "have a try man" << endl;
+void printTheDeque(deque<int>& d)
+{
+	for (deque<int>::iterator it = d.begin();it != d.end(); it++)
+	{
+		cout << *it << " ";
+	}
 }
+//int main() {
+//	cout << "have a try man" << endl;
+//	deque<int>d;
+//	for (int i = 0; i < MAX; ++i)
+//	{
+//		d.push_back(i);
+//	}
+//	printTheDeque(d);
+//}
+
