ac522f4b68a7b28166e58d323df8721016c342dd
CraneTheroux <2640082560@qq.com>
2026-04-20 11:33:29 +0800

    set's custom sort method

--- Project1/day6_4.cpp
+++ Project1/day6_4.cpp
@@ -1,32 +1,32 @@
 ﻿#include<iostream>
 #include<set>
-int main()
-{/*
-	std::set<int>s;
-	
-	std::pair<std::set<int>::iterator, bool>ret = s.insert(10);
-	if (ret.second)
-	{
-		std::cout << "插入成功" << std::endl;
-	}
-	else
-	{
-		std::cout << "插入失败" << std::endl;
-	}*/
-	std::multiset<int>multiset;
-
-
-	multiset.insert(10);
-	multiset.insert(10);
-	auto begin = multiset.begin();
-	for (begin;begin!=multiset.end(); begin++)
-	{
-		std::cout << *begin<<" ";
-		if (begin==multiset.end())
-		{
-			std::cout << *begin <<" ";
-		}
-		
-	}
-	std::cout << ",打印完了";
-}
+//int main()
+//{/*
+//	std::set<int>s;
+//	
+//	std::pair<std::set<int>::iterator, bool>ret = s.insert(10);
+//	if (ret.second)
+//	{
+//		std::cout << "插入成功" << std::endl;
+//	}
+//	else
+//	{
+//		std::cout << "插入失败" << std::endl;
+//	}*/
+//	std::multiset<int>multiset;
+//
+//
+//	multiset.insert(10);
+//	multiset.insert(10);
+//	auto begin = multiset.begin();
+//	for (begin;begin!=multiset.end(); begin++)
+//	{
+//		std::cout << *begin<<" ";
+//		if (begin==multiset.end())
+//		{
+//			std::cout << *begin <<" ";
+//		}
+//		
+//	}
+//	std::cout << ",打印完了";
+//}
