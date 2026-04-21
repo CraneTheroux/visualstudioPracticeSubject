cb9051fc1fcb3aa3a61f4d3b90ad17e2b4d23b49
CraneTheroux <2640082560@qq.com>
2026-04-21 14:15:12 +0800

    map learning

--- Project1/day7.cpp
+++ Project1/day7.cpp
@@ -25,23 +25,22 @@ public:
 		return p1.m_age == p2.m_age ? p1.m_height > p2.m_height:p1.m_age > p2.m_age;
 	}
 };
-int main() {
-
-	std::set<Person,MyCompare>s;
-	Person p(18,"Crane",183);
-	Person p2(19, "sunsun", 180);
-	Person p3(20, "liangzi", 193);
-	Person p4(18, "shalu", 177);
-	s.insert(p);
-	s.insert(p2);
-	s.insert(p3);
-	s.insert(p4);
-	std::set<Person, MyCompare>::iterator beginPtr = s.begin();
-	for (beginPtr;beginPtr != s.end();beginPtr++) {
-		std::cout << *beginPtr << std::endl;
-	}
-
-
+//int main() {
+//
+//	std::set<Person,MyCompare>s;
+//	Person p(18,"Crane",183);
+//	Person p2(19, "sunsun", 180);
+//	Person p3(20, "liangzi", 193);
+//	Person p4(18, "shalu", 177);
+//	s.insert(p);
+//	s.insert(p2);
+//	s.insert(p3);
+//	s.insert(p4);
+//	std::set<Person, MyCompare>::iterator beginPtr = s.begin();
+//	for (beginPtr;beginPtr != s.end();beginPtr++) {
+//		std::cout << *beginPtr << std::endl;
+//	}
 
 
-}
+
+
---
+++ Project1/day8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include<map>
+#include<set>
+class compare
+{
+public:
+	bool operator()(const int val1, const int val2)const
+	{
+		return val1>val2;
+	}
+};
+int main()
+{
+	std::pair<std::string, int>p;
+	std::map<int,std::string>m;
+	m.insert(std::pair<int,std::string>(19,"crane"));
+	m.insert(std::pair<int,std::string>(18,"sun"));
+
+	std::map<int, std::string>::iterator begin1 = m.begin();
+	for (begin1;begin1!=m.end(); begin1++)
+	{
+		std::cout << begin1->first << ", "<<begin1->second<<" ";
+	}
+}
