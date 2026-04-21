a49959ccd447096ef133b569206e569192fcd7f8
CraneTheroux <2640082560@qq.com>
2026-04-21 14:52:21 +0800

    worker practice code 1th progress

---
+++ Project1/day8_2.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#define MAX 10
+#include<map>
+#include<vector>
+
+
+int main()
+{
+	std::vector<std::string>number;
+	for (int i = 0; i < MAX; i++)
+	{
+		std::string str = "ABCDEFGHIJ";
+		std::string temp (1,str[i]);
+		number.push_back(temp);
+	}
+	for (std::vector<std::string>::iterator it = number.begin();it!=number.end()  ; it++)
+	{
+		std::cout << *it<<" ";
+	}
+
+}
